105611d1ff1cd1b92d7c8ffa4fb71f094e9e7347
herok <heroking2081@gmail.com>
2022-10-10 14:57:59 +0500

    4.1. Разбор первой программы. [#159033 #66433]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Black Nigga");
+        System.out.println("08.07.2091");
+    }
+}
