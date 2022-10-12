a2cb95038dd5dab13293223b92c9c8fc25f228e0
herok <heroking2081@gmail.com>
2022-10-12 16:35:56 +0500

    4.3.1. Сокращенный оператор && [#333783 #66361]

---
+++ src/main/java/ru/job4j/condition/Games.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Games {
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if ( allowByParent && hasMoney) {
+            System.out.println("Я могу пойти в компьютерный клуб.");
+        } else {
+            System.out.println("Я не могу.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Games.permission(true, true);
+        Games.permission(true, false);
+        Games.permission(false, true);
+        Games.permission(false, false);
+    }
+}
