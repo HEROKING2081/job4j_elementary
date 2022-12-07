c9549fbc1a0876284489418d66a99ad80495be2e
herok <heroking2081@gmail.com>
2022-12-07 13:56:12 +0500

    0.1. FileOutputStream. [#252488 #66620]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -5,7 +5,8 @@ import java.io.FileOutputStream;
 public class Multiple {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("Multiple.txt")) {
-            out.write(("1 * 1 = 1" + "1 * 2 = 2" + "1 * 3 = 3" + "1 * 4 = 4" + "1 * 5 = 5").getBytes());
+            out.write(("1 * 1 = 1; " + "1 * 2 = 2; " + "1 * 3 = 3; "
+                    + "1 * 4 = 4; " + "1 * 5 = 5").getBytes());
             out.write(System.lineSeparator().getBytes());
         } catch (Exception e) {
             e.printStackTrace();
